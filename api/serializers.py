ae3098a3d45dea2c99e57e7d323ad4168ff65e54
Radeox <dawid.weglarz95@gmail.com>
2020-08-13 17:46:05 +0200

    Serializer for DDT creation

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,61 @@
-from rest_framework.serializers import ChoiceField, ModelSerializer
-from . import models
+from rest_framework.serializers import ModelSerializer, ListSerializer
+from rest_framework import serializers
+from api.models import Container, DDT, User
 
 
 class ContainerSerializer(ModelSerializer):
     class Meta:
-        model = models.Container
-        fields = ('type', 'count')
+        model = Container
+        fields = ['type', 'count']
+
+    def to_representation(self, instance):
+        """Convert container type from key to String"""
+        ret = super().to_representation(instance)
+        ret['type'] = Container.KIND[ret['type']-1][1]
+        return ret
 
 
 class DDTSerializer(ModelSerializer):
-    containers = ContainerSerializer(many=True)
+    containers = serializers.JSONField()
 
     class Meta:
-        model = models.DDT
-        fields = ('containers', 'client', 'date', 'photo')
+        model = DDT
+        fields = ['containers', 'operator', 'client', 'date', 'photo']
+
+    def create(self, validated_data):
+        containers = validated_data.pop('containers')
+        ddt = DDT.objects.create(**validated_data)
+
+        for container in containers:
+            c = ContainerSerializer(data=container)
+            c.is_valid(raise_exception=True)
+            Container.objects.create(
+                ddt_id=ddt.pk,
+                type=container['type'],
+                count=container['count']
+            )
+
+        return ddt
+
+    def to_representation(self, instance):
+        """Convert containers to JSON"""
+        a = []
+        ret = super().to_representation(instance)
+
+        for c in ret['containers'].all():
+            a.append({"type": c.type, "count": c.count})
+        ret['containers'] = a
+        return ret
+
+
+class DDTReadSerializer(ModelSerializer):
+    containers = ContainerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = DDT
+        fields = ['containers', 'operator', 'client', 'date', 'photo']
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        model = models.User
+        model = User
