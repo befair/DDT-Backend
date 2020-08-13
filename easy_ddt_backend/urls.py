50ce70ff9b897373c441250eb65a4cdfadbdc2a1
Radeox <dawid.weglarz95@gmail.com>
2020-08-13 11:24:28 +0200

    Add 'media' for debug

--- easy_ddt_backend/urls.py
+++ easy_ddt_backend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings, urls
+
+
 urlpatterns = [
     path('', admin.site.urls),
     path('api/v1/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) if settings.DEBUG else None
