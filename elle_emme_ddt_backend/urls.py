68ace993ea7ec18886778adab904e303b2f04b44
Radeox <dawid.weglarz95@gmail.com>
2020-08-20 11:30:06 +0200

    Enable reset password views

--- elle_emme_ddt_backend/urls.py
+++ elle_emme_ddt_backend/urls.py
@@ -1,11 +1,30 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf.urls.static import static
 from django.conf import settings, urls
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 urlpatterns = [
     path('', admin.site.urls),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
     path('api/v1/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) if settings.DEBUG else None
