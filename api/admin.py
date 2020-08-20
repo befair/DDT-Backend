3a7a90f1876c8354bc70afe795d8b7280df4fa58
Radeox <dawid.weglarz95@gmail.com>
2020-08-20 15:17:24 +0200

    Change admin field to time

--- api/admin.py
+++ api/admin.py
@@ -20,8 +20,8 @@ class ClientAdmin(ModelAdmin):
 class DDTAdmin(ModelAdmin):
     icon_name = 'local_shipping'
     search_fields = ['serial']
-    readonly_fields = ['creation_time']
-    list_display = ['__str__', 'client', 'date', 'creation_time']
+    readonly_fields = ['time']
+    list_display = ['__str__', 'client', 'date', 'time']
 
     list_filter = ('date', 'client__corporate_name')
 
